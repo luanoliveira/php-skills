7e59e183cd52e489fa7f2aca645a805de4e2db39
Luan Oliveira <luan@mixinternet.com.br>
2017-08-22 13:11:19 -0300

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Inspirational
+ * - https://www.monster.com/career-advice/article/show-your-skills-on-your-it-resume
+ */
+
+$languages = [
+    [
+        'title' => 'Java',
+        'tools' => [],
+        'level' => null
+    ]
+];
+
+?>
+
+<table class="table">
+    <tr>
+        <th>Languages</th>
+        <th></th>
+    </tr>
+</table>
+
+
+Languages
+Java
+XML
+JavaScript
+SQL
+HTML
+UML
